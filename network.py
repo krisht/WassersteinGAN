6f5eecdcf5cdc2679b49b72a5a37575f9987fe71
Krishna <krisht@cooper.edu>
2017-03-05 18:44:21 -0500

    Added stuff

--- network.py
+++ network.py
@@ -7,6 +7,15 @@ lrn_rate = 1e-4
 bth_size = 64
 dataset = 'celeba'
 
+def prelu(_x, name):
+    alphas = tf.get_variable(name,  _x.get_shape()[-1], initializer=tf.constant_initializer(0.0), dtype=tf.float32)
+    pos = tf.nn.relu(_x)
+    neg = alphas*(_x - abs(_x)) * 0.5
+
+    tf.add_to_collection('model_vars', alphas)
+
+    return pos + neg
+
 
 def gen(gen_img):
     gen_img = slim.fully_connected(gen_img, 4 * 4 * 1024, normalizer_fn=slim.batch_norm, activation_fn=tf.identity,
@@ -17,13 +26,13 @@ def gen(gen_img):
 
     gen_img = slim.convolution2d_transpose(gen_img, 512, 5, stride=2, normalizer=slim.batch_norm,
                                            activation_fn=tf.identity, scope='gen_l2')
-    gen_img = tf.nn.relu(gen_img)
+    gen_img = prelu(gen_img, 'gen_l2_prelu')
 
     # Deconv Layer 2
 
     gen_img = slim.convolution2d_transpose(gen_img, 256, 5, stride=2, normalizer_fn=slim.batch_norm,
                                            activation_fn=tf.identity, scope='gen_l3')
-    gen_img = tf.nn.relu(gen_img)
+    gen_img = prelu(gen_img, 'gen_l3_prelu')
 
     # Deconv Layer 3
 
@@ -42,19 +51,19 @@ def cri(input_image, reuse_scope=False):
         # Conv Layer 1
 
         y_hat = slim.convolution(input_image, 64, 5, stride=2, activation_fn=tf.identity, scope='crit_l1')
-        y_hat = tf.nn.relu(y_hat)
+        y_hat = prelu(y_hat, 'crit_l1_prelu')
 
         # Conv Layer 2
 
         y_hat = slim.convolution(y_hat, 128, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity,
                                  scope='crit_l2')
-        y_hat = tf.nn.relu(y_hat)
+        y_hat = prelu(y_hat, 'crit_l2_prelu')
 
         # Conv Layer 3
 
         y_hat = slim.convolution(y_hat, 256, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity,
                                  scope='crit_l3')
-        y_hat = slim.relu(y_hat)
+        y_hat = prelu(y_hat, 'crit_l3_prelu')
 
         # Conv Layer 4
 
