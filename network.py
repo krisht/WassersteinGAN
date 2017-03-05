71cc196ec471327ece630a6bd4b78aa398ab9922
Krishna <krisht@cooper.edu>
2017-03-05 18:37:10 -0500

    Added initial network stuff

--- network.py
+++ network.py
@@ -1,28 +1,67 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
-import sys
 
-
-data_dir ='/data/images/'
+data_dir = '/data/images/'
 chkpt_dir = '/chkpts/'
 lrn_rate = 1e-4
 bth_size = 64
 dataset = 'celeba'
 
-def prelu(x):
+
+def gen(gen_img):
+    gen_img = slim.fully_connected(gen_img, 4 * 4 * 1024, normalizer_fn=slim.batch_norm, activation_fn=tf.identity,
+                                   scope='gen_l1')
+    gen_img = tf.reshape(gen_img, [bth_size, 4, 4, 1024])
+
+    # Deconv Layer 1 for Generation
+
+    gen_img = slim.convolution2d_transpose(gen_img, 512, 5, stride=2, normalizer=slim.batch_norm,
+                                           activation_fn=tf.identity, scope='gen_l2')
+    gen_img = tf.nn.relu(gen_img)
+
+    # Deconv Layer 2
+
+    gen_img = slim.convolution2d_transpose(gen_img, 256, 5, stride=2, normalizer_fn=slim.batch_norm,
+                                           activation_fn=tf.identity, scope='gen_l3')
+    gen_img = tf.nn.relu(gen_img)
+
+    # Deconv Layer 3
+
+    gen_img = slim.convolution2d_transpose(gen_img, 128, 5, stride=2, normalizer_fn=slim.batch_norm,
+                                           activation_fn=tf.identity, scope='gen_l4')
+    gen_img = tf.nn.tanh(gen_img)
+
+    tf.add_to_collection('model_vars', gen_img)
+
+    return gen_img
 
 
+def cri(input_image, reuse_scope=False):
+    sc = tf.get_variable_scope()
+    with tf.variable_scope(sc, reuse=reuse_scope):
+        # Conv Layer 1
 
-def gen(y_hat):
-    y_hat = slim.fully_connected(yhat, 4*4*1024, normalizer_fn=slim.batch_norm, activation_fn=tf.identity, scope='gen_l1')
-    y_hat = tf.reshape(y_hat, [bth_size, 4, 4, 1024]);
+        y_hat = slim.convolution(input_image, 64, 5, stride=2, activation_fn=tf.identity, scope='crit_l1')
+        y_hat = tf.nn.relu(y_hat)
 
-    # Conv Layer 1 for Generation
+        # Conv Layer 2
 
-    y_hat = slim.convolution2d_transpose(y_hat, 512, 5, stride=2, normalizer=slim.batch_norm, activation_fn=tf.identity, scope='gen_l2')
-    y_hat = tf.nn.relu(y_hat)
+        y_hat = slim.convolution(y_hat, 128, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity,
+                                 scope='crit_l2')
+        y_hat = tf.nn.relu(y_hat)
 
+        # Conv Layer 3
 
-def cri(input_image, batch_size, reuse = False):
+        y_hat = slim.convolution(y_hat, 256, 5, stride=2, normalizer_fn=slim.batch_norm, activation_fn=tf.identity,
+                                 scope='crit_l3')
+        y_hat = slim.relu(y_hat)
 
+        # Conv Layer 4
 
+        y_hat = slim.convolution(y_hat, 256, 5, stride=2, normalizer=slim.batch_norm, activation_fn=tf.identity,
+                                 scope='crit_l4')
+        y_hat = slim.convolution(y_hat, 1, 4, stride=2, activation_fn=tf.identity, scope='crit_l5')
+
+        tf.add_to_collection('model_vars', y_hat)
+
+        return y_hat
