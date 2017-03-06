339270c9eda96ab67e3bf75c7c04c39b69dbf75b
Krishna <krisht@cooper.edu>
2017-03-06 16:32:25 -0500

    Not working yet

--- train_GAN.py
+++ train_GAN.py
@@ -1,11 +1,31 @@
 from network import WGAN
 import numpy as np
-import scipy.misc as science
+from scipy.misc import imsave, imshow
+from tensorflow.examples.tutorials.mnist import input_data
+import warnings
+
+warnings.filterwarnings('ignore')
+
+mnist = input_data.read_data_sets("data/", one_hot=True)
 
 test = WGAN()
-test.train_iteration(1, 2, 100, [np.reshape(range(784), newshape=[28,28,1])])
-images = test.generate_image(10)
-for image, number in zip(images, range(len(images))):
-    science.imsave("out" + str(number), image, 'JPEG')
+
+def genPics(nPics):
+    images = test.generate_image(10)
+    for image, number in zip(images, range(len(images))):
+        imsave("out" + str(number),  image, 'JPEG')
+
+
+def train(nEpoch, batch_size):
+    for kk in range(nEpoch):
+        for samp in range(int(500/batch_size)):
+            print("Epoch:", kk, "Sample:", samp)
+            images, _  = mnist.train.next_batch(batch_size)
+            images = images.reshape((batch_size, 28, 28, 1))
+            test.train_iteration(10, images)
+        genPics(10)
+
+
+train(5, 10)
 
 
