c2976ff99f7360aa081c434c213859c7ccb11080
zhengqixi <zhengqixi@github.com>
2017-03-10 21:02:19 -0500

    working on bilinear

--- bilinear.py
+++ bilinear.py
@@ -1,44 +1,17 @@
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-import os
+import tensorflow.contrib.slim as slim
 
-mnist = input_data.read_data_sets('../data/', one_hot=True)
-
-x,y = mnist.train.next_batch(1)
-
-x = np.reshape(x, (1, 28, 28,1))
-
-X = tf.placeholder(tf.float32, shape=[None, 28, 28, 1])
-
-# it's height, width in TF - not width, heig0ht
-
-scale = 100
-new_height = int(round(28* scale))
-new_width = int(round(28 * scale))
-# after TF version r0.11
-resized = tf.image.resize_bicubic(X, [new_height, new_width])
-
-print(resized)
-
-tf.global_variables_initializer()
-
-sess = tf.Session()
-
-out = sess.run(resized, feed_dict={X: x})
-
-print(out.shape)
-
-plt.figure(1)
-
-plt.imshow(out.reshape(new_height,new_width), cmap='Greys_r')
+def generator(input, output_dims, input_dims):
+    net = slim.layers.fully_connected(input, num_outputs=20)
+    net = slim.layers.fully_connected(net, num_outputs=25)
+    net = tf.reshape(net, shape=[None, 5, 5, 1])
+    net = tf.image.resize_bilinear(net, size=[15,15])
+    net = slim.layers.convolution(net, num_outputs=10)
+    net = tf.image.resize_bilinear(net, size=[28,28])
+    return net
 
 
-plt.figure(2)
+def critic(input, input_dims, reuse_scope=False):
+    net = tf.reshape(input, shape=[None, 28, 28, 1])
 
 
-plt.imshow(x.reshape(28, 28), cmap='Greys_r')
-
-plt.show()
